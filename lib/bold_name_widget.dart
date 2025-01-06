1b9ced983786905bc167e28963f00d8adbef0e7c
Alexey <Gridbugkiller@gmail.com>
2025-01-06 15:03:28 +0700

    Replaced container with Card, switched to Theme usage, added some accesibility

--- lib/bold_name_widget.dart
+++ lib/bold_name_widget.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 class BoldNameWidget extends StatelessWidget {
@@ -6,22 +7,25 @@ class BoldNameWidget extends StatelessWidget {
     required this.name,
   });
 
-  final String name;
+  final WordPair name;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.deepOrange,
-      ),
-      padding: const EdgeInsets.all(8),
-      child: Text(
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
+    final theme = Theme.of(context);
+
+    final nameStyle = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          name.asLowerCase,
+          style: nameStyle,
+          semanticsLabel: "${name.first} ${name.second}",
         ),
-        name,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -41,18 +41,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            BoldNameWidget(name: appState.current.asLowerCase),
+            BoldNameWidget(name: appState.current),
             SizedBox(height: 16),
             ElevatedButton(
                 onPressed: () {
